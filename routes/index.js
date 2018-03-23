336fd8e5fa5a7952ec8f0b1c211307822dc42fde
Alex Michal <you@example.com>
2018-03-23 01:10:11 -0700

    Added EJS and added ability to load the main page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' }); // dynamically adding to our index file  
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+  // res.sendFile('../views/index.html' , { root : __dirname});
 });
+
+
 
 module.exports = router;
